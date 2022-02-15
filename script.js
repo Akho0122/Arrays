ec31cb31807c7346e2b4a01a9ee49e6917d813a9
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-15 19:34:04 +0500

    Array methods part 1

--- script.js
+++ script.js
@@ -74,3 +74,43 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(1, -1));
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE
+console.log(arr.splice(2));
+console.log(arr.splice(1, 2));
+arr.splice(-1);
+console.log(arr);
+
+// REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+console.log(letters.join(' - '));
+
+const arr3 = [23, 11, 64];
+console.log(arr3[0]);
+console.log(arr3.at(0));
+console.log(arr3[arr3.length - 1]);
+console.log(arr3.slice(-1)[0]);
+console.log(arr3.at(-1));
+
+console.log('Jonas'.at(0));
+console.log('Jonas'.at(-1));
