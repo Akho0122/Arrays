49d54abfcf525138b925378d12a56504bb53b9ea
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-16 18:23:11 +0500

    Reduce and filter methods

--- script.js
+++ script.js
@@ -83,9 +83,8 @@ const createUsernames = function (accs) {
       .join('');
   });
 };
-
 createUsernames(accounts);
-console.log(accounts); // stw
+console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -223,3 +222,36 @@ console.log(accounts); // stw
 //   // }
 // );
 // console.log(movementsDescriptions);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(deposits);
+
+const depositFor = [];
+for (const mov of movements) {
+  if (mov >= 0) {
+    depositFor.push(mov);
+  }
+}
+console.log(depositFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + cur;
+}, 0);
+console.log(balance);
+
+const balance1 = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance1);
+
+let balance2 = 0;
+for (const mov of movements) {
+  balance2 += mov;
+}
+console.log(balance2);
